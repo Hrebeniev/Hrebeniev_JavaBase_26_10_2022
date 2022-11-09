93e3e7f5eddc305b75c562b7295474fb359e7d1e
Hrebeniev Nick <nick.hrebeniev@gmail.com>
2022-11-09 23:05:39 +0200

    added Hello

--- src/main/java/com/hillel/hrebeniev/homeworks/homework_3/Main.java
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_3/Main.java
@@ -1,2 +1,7 @@
-package com.hillel.hrebeniev.homeworks.homework_3;public class Main {
+package com.hillel.hrebeniev.homeworks.homework_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
