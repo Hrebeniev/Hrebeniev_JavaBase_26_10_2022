3c26b7b03320a009926f5b477d9188c1918a8125
Hrebeniev Nick <nick.hrebeniev@gmail.com>
2022-11-29 15:28:17 +0200

    added Hello

---
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_4/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.hrebeniev.homeworks.homework_4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 4;
+    int volume = a*b*c;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        int lenght = (a+b+c) * 4;
+        System.out.println("Довжина сторін = " + lenght);
+    }
+}
