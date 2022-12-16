2270fc53ce1106d4dbb7409084e220d4a4bd3e17
Hrebeniev Nick <nick.hrebeniev@gmail.com>
2022-12-16 12:07:38 +0200

    Готовое ДЗ

--- src/main/java/com/hillel/hrebeniev/homeworks/homework_8/Main.java
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_8/Main.java
@@ -1,12 +1,21 @@
 package com.hillel.hrebeniev.homeworks.homework_8;
 
+import sun.font.TrueTypeFont;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (i != 4 && j != 4)
-                    System.out.println("Шаттл " + i + "" + j);
+
+        int i = 1;
+        int ii = 1;
+        while (i > 0 && ii <= 100) {
+            int first = i / 100;
+            int second = i / 10 - first * 10;
+            int third = i - first * 100 - second * 10;
+            if (first != 4 && second != 4 && third != 4 && first != 9 && second != 9 && third != 9) {
+                System.out.println("Шаттл " + i);
+                ii++;
             }
+            i++;
         }
     }
 }
