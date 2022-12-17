bf56b4e70c213721f5a2b8b30757868f72bd1312
Hrebeniev Nick <nick.hrebeniev@gmail.com>
2022-12-17 22:23:39 +0200

    Готовое ДЗ

---
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_10/Main.java
@@ -0,0 +1,35 @@
+package com.hillel.hrebeniev.homeworks.homework_10;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] first = new int[7];
+        int[] second = new int[7];
+        for (int i = 0; i < first.length; i++) {
+            first[i] = (int) (Math.random() * 10);
+            second[i] = (int) (Math.random() * 10);
+        }
+        System.out.println("Массивы до сортировки:");
+        System.out.println(Arrays.toString(first));
+        System.out.println(Arrays.toString(second));
+
+        Arrays.sort(first);
+        Arrays.sort(second);
+
+        System.out.println();
+        System.out.println("Массивы после сортировки:");
+        System.out.println(Arrays.toString(first));
+        System.out.println(Arrays.toString(second));
+        System.out.println();
+
+        int match = 0;
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] == second[i]) {
+                match++;
+            }
+        }
+        System.out.println("Количество совпадений: " + match);
+
+    }
+}
