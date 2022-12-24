65cbe8a20d003c382d4d419e749c74a2e3f63a9d
Hrebeniev Nick <nick.hrebeniev@gmail.com>
2022-12-24 10:10:23 +0200

    Готовое ДЗ

--- src/main/java/com/hillel/hrebeniev/homeworks/homework_12/Main.java
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_12/Main.java
@@ -4,11 +4,9 @@ import static com.hillel.hrebeniev.homeworks.homework_12.Person.personInfo;
 
 public class Main {
     public static void main(String[] args) {
-        personInfo("Will", "Smith", "New York", "2936729462846");
-        System.out.println();
-        personInfo("Jackie", "Chan", "Shanghai", "12312412412");
-        System.out.println();
-        personInfo("Sherlock", "Holmes", "London", "37742123513");
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Sherlock", "Holmes", "London", "37742123513"));
+        System.out.println(personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
     }
 
 }
--- src/main/java/com/hillel/hrebeniev/homeworks/homework_12/Person.java
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_12/Person.java
@@ -1,8 +1,11 @@
 package com.hillel.hrebeniev.homeworks.homework_12;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Person {
-    static void personInfo(String name, String surname, String city, String phoneNumber) {
-        System.out.println("Позвонить гражданину " + name + " " + surname +
-                " из города " + city + " можно по номеру: " + phoneNumber);
+    public static String personInfo(String name, String surname, String city, String phoneNumber) {
+        String person = "Позвонить гражданину " + name + " " + surname +
+                " из города " + city + " можно по номеру: " + phoneNumber;
+        return person;
     }
 }
